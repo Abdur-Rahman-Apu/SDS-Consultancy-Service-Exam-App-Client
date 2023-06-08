95009949cc30f12286d0ecfbe9d31e90d4abba48
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-06-09 01:36:52 +0600

    question format

--- src/Pages/Exam/Exam.jsx
+++ src/Pages/Exam/Exam.jsx
@@ -2,62 +2,74 @@ import { useEffect, useState } from "react";
 import Questions from "../../assets/JsonFiles/Question.json"
 
 const ExamPage = () => {
-  const [Marked, setMarked] = useState([])
-var SelcetedData=[];
-  const handleChecked = (data) => {
-    setMarked(data)
-  }
-  const Selected = "text-white bg-green-600 border-2 border-green-600 font-semibold"
-console.log(Marked,SelcetedData,"Marked,SelectedData")
-  return (
-    <div className="max-w-3xl mx-auto">
-      {/* Exam Title */}
-      <div className="flex justify-between items-center my-10 px-10">
-        <h2>Course Name:</h2>
-        <h2>Exam Duration:</h2>
-        <h2>Time Remaining:</h2>
-      </div>
-      {/* Exam Body */}
-      {Questions.map((question,index )=> {
-        return (
-          <div key={question.question}>
+	const [userAnswers, setUserAnswers] = useState({});
+	const [OptionStyle,setOptionStyle]=useState(null)
 
-            <div className="my-10 bg-gray-100 shadow-xl px-10 py-5 rounded-lg">
-              <p className="text-gray-600">Question-{index+1}</p>
-              <h3 className="mt-3 mb-5 font-extrabold">{question.question}</h3>
+	const handleAnswerChange = (questionId, selectedOption,option) => {
+		setUserAnswers((prevAnswers) => ({
+			...prevAnswers,
+			[questionId]: selectedOption,
+		}));
+		setOptionStyle(option);
 
-              <div className="space-y-5">
-                <p onClick={() => handleChecked(question.A)} className={`${Marked === question.A ? Selected : "border border-gray-300"}
-                    p-4 rounded-md flex items-center cursor-pointer`}>
-                  <span className={`p-3 py-1 rounded-md ${Marked === question.A ? "bg-green-600 text-white ml" : ""}`}>A</span>
-                  <span className={`${Marked === question.A ? "border-none" : "border-l-2"} pl-3`}>{question.A}</span>
-                </p>
+	};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Do something with the userAnswers, e.g., send them to the server
+    console.log(userAnswers);
+  };
 
-                <p onClick={() => handleChecked(question.B)} className={`${Marked === question.B ? Selected : "border border-gray-300"} p-4 rounded-md flex items-center cursor-pointer`}>
-                  <span className={`p-3 py-1 rounded-md ${Marked === question.B ? "bg-green-600 text-white" : ""}`}>B</span>
-                  <span className={`${Marked === question.B ? "border-none" : "border-l-2"} pl-3`}>{question.B}</span>
-                </p>
+	return (
+		<div className="max-w-3xl mx-auto py-24 font-Roboto">
+			{/* Exam Title */}
+			<div className="flex justify-between items-center my-10 px-10">
+				<h2>Course Name:</h2>
+				<h2>Exam Duration:</h2>
+				<h2>Time Remaining:</h2>
+			</div>
+			{/* Exam Body */}
+			{Questions.map((question, index) => {
+				const Options = [question.A, question.B, question.C, question.D]
 
-                <p onClick={() => handleChecked(question.C)} className={`${Marked === question.C ? Selected : "border border-gray-300"} p-4 rounded-md flex items-center cursor-pointer`}>
-                  <span className={`p-3 py-1 rounded-md ${Marked === question.C ? "bg-green-600 text-white" : ""}`}>C</span>
-                  <span className={`${Marked === question.C ? "border-none" : "border-l-2"} pl-3`}>{question.C}</span>
-                </p>
+				return (
+					<div key={question.question}>
 
-                <p onClick={() => handleChecked(question.D)} className={`${Marked === question.D ? Selected : "border border-gray-300"} p-4 rounded-md flex items-center cursor-pointer`}>
-                  <span className={`px-3 py-1 rounded-md ${Marked === question.D ? "bg-green-600 text-white" : ""}`}>D</span>
-                  <span className={`${Marked === question.D ? "border-none" : "border-l-2"} pl-3`}>{question.D}</span>
-                </p>
-              </div>
-            </div>
-          </div>
-        )
-      })}
-      {
+						<div className="my-10 bg-gray-100 shadow-xl px-10 py-5 rounded-lg">
+							<p className="text-gray-600">Question-{index + 1}</p>
+							<h3 className="mt-3 mb-5 font-extrabold font-IBM text-xl">{question.question}</h3>
 
 
-      }
-    </div>
-  )
+							<div className="space-y-5">
+									{
+									Options.map((option,optionIndex) => {
+										const OptionName = (optionIndex == 0 && "A") || (optionIndex == 1 && "B") || (optionIndex == 2 && "C") || (optionIndex == 3 && "D");
+											return	(
+											<p
+														key={option}
+														onClick={() => handleAnswerChange(index, OptionName,option)}
+														className={`p-4 rounded-md flex items-center cursor-pointer ${userAnswers[index] === OptionName ? 'bg-green-600 text-white' : 'border border-gray-300'
+															} ${OptionStyle===option
+																?"font-extrabold":"font-normal"}`}
+													>
+														<span className="p-3 py-1 rounded-md bg-green-600 text-white">{OptionName}</span>
+														<span className={`pl-3`}>{option}</span>
+													</p>)
+												})
+												}
+							</div>
+						</div>
+						
+					</div>
+				)
+			})}
+			<button
+        onClick={handleSubmit}
+        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+      >
+        Submit Answers
+      </button>
+		</div>
+	)
 };
 
 export default ExamPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        DidplayFair:"Playfair Display, serif",
+        Roboto:"Roboto Condensed, sans-serif",
+        IBM:"IBM Plex Sans, sans-serif"
+      }
+    },
   },
   plugins: [require("daisyui")],
 };
