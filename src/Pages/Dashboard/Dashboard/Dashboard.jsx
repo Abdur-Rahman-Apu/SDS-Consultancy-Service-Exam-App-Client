043b4a48585452afdf096402e08b379fbcaddcc2
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-06-10 19:38:54 +0600

    Employee dashboard is added

--- src/Pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard/Dashboard.jsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
   return (
     <div>
       <h1 className="text-center mt-16 mb-10 font-roboto text-3xl md:text-4xl font-bold">
-        Welcome to the Dashboard
+        Welcome to the <span className="text-[#2DB6BB]">Dashboard</span>
       </h1>
 
       <div>{role === "admin" && <AdminDashboard />}</div>
