e5d86d2ebab781c4f245a5bd5e460232048781d0
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-06-10 06:00:43 +0600

    Successfully get json file & receive data

--- src/Pages/Dashboard/Admin/Courses/AddCourseQuestion/AddCourseQuestion.jsx
+++ src/Pages/Dashboard/Admin/Courses/AddCourseQuestion/AddCourseQuestion.jsx
@@ -1,4 +1,34 @@
+import { useForm } from "react-hook-form";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const AddCourseQuestion = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const courseName = useParams();
+
+  // Read JSON file and insert into the database
+  const onSubmit = (data) => {
+    console.log(data);
+
+    const jsonFile = data.file[0];
+
+    new Response(jsonFile).json().then(
+      (data) => {
+        console.log(data);
+      },
+      (err) => {
+        console.log(err);
+        toast.error("Please upload a JSON file");
+      }
+    );
+  };
+
   return (
     <div>
       <h1 className="text-4xl font-roboto font-bold text-center mt-16">
@@ -7,24 +37,40 @@ const AddCourseQuestion = () => {
 
       {/* upload a json file  */}
 
-      <form className="  mt-16 w-[50%] mx-auto">
-        <label
-          className="block mb-2 text-lg font-bold font-roboto text-gray-900"
-          htmlFor="file_input"
-        >
-          Upload a JSON file
-        </label>
-        <input
-          type="file"
-          className="file-input file-input-bordered  w-full max-w-xs"
-        />
+      <form
+        className="mt-16 mx-5 flex justify-center"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="mr-3">
+          <label
+            className="block mb-2 text-lg font-bold font-roboto text-gray-900"
+            htmlFor="file_input"
+          >
+            Upload a JSON file
+          </label>
+          <input
+            type="file"
+            className="file-input file-input-bordered  w-full max-w-xs"
+            {...register("file", { required: true })}
+            aria-invalid={errors.file ? "true" : "false"}
+          />
+        </div>
 
         <input
           type="submit"
           value="Upload the file"
-          className="px-[8px] py-[10px] font-roboto font-bold rounded-lg text-white ml-5"
+          className=" mt-10 px-[8px] py-[10px] font-roboto font-bold rounded-lg text-white cursor-pointer"
         />
       </form>
+
+      {errors.file?.type === "required" && (
+        <p
+          role="alert"
+          className="text-center text-red-500 font-roboto font-bold text-sm mt-2"
+        >
+          JSON file is required
+        </p>
+      )}
     </div>
   );
 };
