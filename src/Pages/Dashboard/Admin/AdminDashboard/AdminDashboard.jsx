16f7eb76e5a753cf6ae828e47c1ef27382a98a77
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-06-10 17:57:19 +0600

    Admin dashboard design is completed

---
+++ src/Pages/Dashboard/Admin/AdminDashboard/AdminDashboard.jsx
@@ -0,0 +1,36 @@
+import { BsFillPeopleFill, BsBookHalf } from "react-icons/bs";
+import style from "./AdminDashboard.module.css";
+import useEmployees from "../../../../CustomHook/useEmployees/useEmployees";
+import useCourses from "../../../../CustomHook/useCourses/useCourses";
+
+const AdminDashboard = () => {
+  const [employees] = useEmployees();
+  const [courses] = useCourses();
+  return (
+    <div className="flex flex-col items-center  md:flex-row justify-evenly">
+      {/* card 1  */}
+      <div className="card w-64 bg-base-100 shadow-xl mb-10 md:mb-0">
+        <div className="card-body flex flex-col items-center">
+          <div className={`bg-[#80D8D3] w-fit p-3 rounded-[50%] ${style.box1}`}>
+            <BsFillPeopleFill className=" text-xl text-white" />
+          </div>
+          <p className="font-roboto font-semibold">Total Employees</p>
+          <h2 className="card-title">{employees?.length}</h2>
+        </div>
+      </div>
+
+      {/* card 2 */}
+      <div className="card w-64 bg-base-100 shadow-xl">
+        <div className="card-body flex flex-col items-center">
+          <div className={`bg-[#7CA3DD] w-fit p-3 rounded-[50%] ${style.box2}`}>
+            <BsBookHalf className=" text-xl text-white" />
+          </div>
+          <p className="font-roboto font-semibold">Total Courses</p>
+          <h2 className="card-title">{courses?.length}</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
--- src/Pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard/Dashboard.jsx
@@ -1,9 +1,18 @@
+import { useContext } from "react";
+import { AuthContext } from "../../../Context/AuthProvider";
+import AdminDashboard from "../Admin/AdminDashboard/AdminDashboard";
+
 const Dashboard = () => {
+  const { employeeInfo } = useContext(AuthContext);
+  const { role } = employeeInfo;
+  console.log(role);
   return (
     <div>
       <h1 className="text-center mt-16 mb-10 font-roboto text-3xl md:text-4xl font-bold">
         Welcome to the Dashboard
       </h1>
+
+      <div>{role === "admin" && <AdminDashboard />}</div>
     </div>
   );
 };
